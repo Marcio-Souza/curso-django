fdf838ed84cc8cc3cac5546df1e73aa2f860467c
Marcio-Souza <wache2k@gmail.com>
2020-07-09 13:42:11 -0300

    Wrapped url in app
    close #70

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -4,10 +4,9 @@ from django.urls import reverse
 from pypro.django_assertions import assert_contains
 
 
-
 @pytest.fixture
 def resp(client):
-    resp = client.get(reverse('home'))
+    resp = client.get(reverse('base:home'))
     return resp
 
 
@@ -20,5 +19,4 @@ def test_title(resp):
 
 
 def test_home_link(resp):
-    assert_contains(resp, f'href="{reverse("home")}">Python Pro</a>')
-
+    assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
