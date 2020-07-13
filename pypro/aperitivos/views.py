d3386062032aa82f6192ab9435c725455f09f227
Marcio-Souza <wache2k@gmail.com>
2020-07-13 12:46:34 -0300

    Added new video in the same template
    close #84

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'video_id': 437255932},
+        'instalacao-windows': {'titulo': 'Video Aperitivo: Instalação Windows', 'video_id': 437893110}
+    }
+    video = videos[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
