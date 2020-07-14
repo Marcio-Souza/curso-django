e80948b6e5d6d7f0782482b30d06f4539f68e09a
Marcio-Souza <wache2k@gmail.com>
2020-07-14 15:52:27 -0300

    Changed to access attribute in template
    close #89

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 
-# Listagem de vídeos
+
+class Video:
+    def __init__(self, slug, titulo, video_id):
+        self.titulo = titulo
+        self.slug = slug
+        self.video_id = video_id
+
+
 videos = [
-    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'video_id': 437255932},
-    {'slug': 'instalacao-windows', 'titulo': 'Video Aperitivo: Instalação Windows',
-     'video_id': 437893110}
+    Video('motivacao', 'Video Aperitivo: Motivação', 437255932),
+    Video('instalacao-windows', 'Video Aperitivo: Instalação Windows', 437893110)
 ]
 
-videos_dct = {dct['slug']: dct for dct in videos}
+videos_dct = {v.slug: v for v in videos}
 
 
 def video(request, slug):
