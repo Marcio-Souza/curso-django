49648d7378dd64ff447b0da39e3b38f4e7e31b43
Marcio-Souza <wache2k@gmail.com>
2020-07-14 15:16:29 -0300

    Added Filters and loop
    close #87

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
+# Listagem de vídeos
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'video_id': 437255932},
+    {'slug': 'instalacao-windows', 'titulo': 'Video Aperitivo: Instalação Windows',
+     'video_id': 437893110}
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
 
 def video(request, slug):
-    videos = {
-        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'video_id': 437255932},
-        'instalacao-windows': {'titulo': 'Video Aperitivo: Instalação Windows', 'video_id': 437893110}
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
