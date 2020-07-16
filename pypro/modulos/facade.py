963a26d429fe340aacd78fbb27b9783199e634fb
Marcio-Souza <wache2k@gmail.com>
2020-07-16 18:11:01 -0300

    Created Processing context
    close #110

--- pypro/modulos/facade.py
+++ pypro/modulos/facade.py
@@ -6,6 +6,7 @@ from pypro.modulos.models import Modulo
 def listar_modulos_ordenados() -> List[Modulo]:
     """
         Lista todos os modulos ordenados por título
+
     :return:
     """
-    return list(Modulo.objects.order_by('titulo').all())
+    return list(Modulo.objects.order_by('order').all())
