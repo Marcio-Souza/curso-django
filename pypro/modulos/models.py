24d020a61de2e7d73fcd3589a94f54f318228284
Marcio-Souza <wache2k@gmail.com>
2020-07-17 20:47:51 -0300

    Created Processing context
    close #110

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from ordered_model.models import OrderedModel
 
 
@@ -6,9 +7,14 @@ class Modulo(OrderedModel):
     titulo = models.CharField(max_length=64)
     publico = models.TextField()
     descricao = models.TextField()
+    slug = models.SlugField(unique=True)
 
     class Meta(OrderedModel.Meta):
         pass
 
     def __str__(self):
         return self.titulo
+
+    @property
+    def get_absolute_url(self):
+        return reverse('modulos:detalhe', kwargs={'slug': self.slug})
---
+++ pypro/modulos/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+
+def detalhe(request, slug):
+    return render(request)
