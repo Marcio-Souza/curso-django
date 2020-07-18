5e742c7c192a4b8860ac8a87e6183ee310117a37
Marcio-Souza <wache2k@gmail.com>
2020-07-17 21:18:22 -0300

    Created Processing context
    close #113

--- pypro/modulos/tests/test_facade.py
+++ pypro/modulos/tests/test_facade.py
@@ -1,13 +1,4 @@
-import pytest
-from model_bakery import baker
-
 from pypro.modulos import facade
-from pypro.modulos.models import Modulo
-
-
-@pytest.fixture
-def videos(db):
-    return [baker.make(Modulo, titulo=s) for s in 'Antes Depois'.split()]
 
 
 def test_listar_modulos_ordenados(modulos):
