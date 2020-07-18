5d06f67b2c264ea279ff92436e3f37ed4963d9cd
Marcio-Souza <wache2k@gmail.com>
2020-07-17 21:18:22 -0300

    Created Processing context
    close #113

--- pypro/modulos/tests/test_aba_de_modulos.py
+++ pypro/modulos/tests/test_aba_de_modulos.py
@@ -1,14 +1,7 @@
 import pytest
 from django.urls import reverse
-from model_bakery import baker
 
 from pypro.django_assertions import assert_contains
-from pypro.modulos.models import Modulo
-
-
-@pytest.fixture
-def modulos(db):
-    return baker.make(Modulo, 2)
 
 
 @pytest.fixture
@@ -20,3 +13,8 @@ def resp(client, modulos):
 def test_titulos_dos_modulos(resp, modulos):
     for modulo in modulos:
         assert_contains(resp, modulo.titulo)
+
+
+def test_link_dos_modulos(resp, modulos):
+    for modulo in modulos:
+        assert_contains(resp, modulo.get_absolute_url())
