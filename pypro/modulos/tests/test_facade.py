79f9b7054d493334fc8831a547f3b08431650503
MARCIO GARCIA DE SOUZA <wache2k@gmail.com>
2020-07-22 20:30:09 -0300

    Update test_facade.py

--- pypro/modulos/tests/test_facade.py
+++ pypro/modulos/tests/test_facade.py
@@ -4,3 +4,4 @@ from pypro.modulos import facade
 # fachada para testes
 def test_listar_modulos_ordenados(modulos):
     assert list(sorted(modulos, key=lambda modulo: modulo.titulo)) == facade.listar_modulos_ordenados()
+    
