c53aaf856f141a16ae5a29e8c7925a4df6c0e0a3
Marcio-Souza <wache2k@gmail.com>
2020-07-22 15:55:12 -0300

    Installed IPython and Django-Extensions
    close #126

--- pypro/modulos/tests/test_facade.py
+++ pypro/modulos/tests/test_facade.py
@@ -1,5 +1,6 @@
 from pypro.modulos import facade
 
 
+# fachada para testes
 def test_listar_modulos_ordenados(modulos):
     assert list(sorted(modulos, key=lambda modulo: modulo.titulo)) == facade.listar_modulos_ordenados()
