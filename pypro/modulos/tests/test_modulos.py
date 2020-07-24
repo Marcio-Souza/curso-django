9ecf5f05b0b369bb1d0313412dc4e7c05e63bb59
Marcio-Souza <wache2k@gmail.com>
2020-07-24 16:13:02 -0300

    Corrected MailChimp
    close #132

--- pypro/modulos/tests/test_modulos.py
+++ pypro/modulos/tests/test_modulos.py
@@ -1,7 +1,14 @@
 import pytest
 from django.urls import reverse
+from model_bakery import baker
 
 from pypro.django_assertions import assert_contains
+from pypro.modulos.models import Modulo
+
+
+@pytest.fixture
+def modulos(db):
+    return baker.make(Modulo, 2)
 
 
 @pytest.fixture
--- pypro/modulos/urls.py
+++ pypro/modulos/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from pypro.modulos.views import detalhe, aula
+from pypro.modulos.views import detalhe, aula, indice
 
 app_name = 'modulos'
 urlpatterns = [
     path('<slug:slug>', detalhe, name='detalhe'),
     path('aula/<slug:slug>', aula, name='aula'),
+    path('', indice, name='indice'),
 
 ]
