d16184d41e44d0670cfeb9f1f2aaa71b3b62075b
Marcio-Souza <wache2k@gmail.com>
2020-07-27 18:01:48 -0300

    Created login form
    close #139

--- pypro/urls.py
+++ pypro/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('pypro.base.urls')),
+    path('contas/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
     path('aperitivos/', include('pypro.aperitivos.urls')),
     path('modulos/', include('pypro.modulos.urls')),
     path('turmas/', include('pypro.turmas.urls')),
