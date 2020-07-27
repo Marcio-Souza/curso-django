6cd23457032e9f76aba528ca8e88d9ba7ba95cdd
Marcio-Souza <wache2k@gmail.com>
2020-07-26 21:00:12 -0300

    Created relationship between Class and Student
    close #137

--- pypro/modulos/tests/test_facade.py
+++ pypro/modulos/tests/test_facade.py
@@ -1,13 +1,4 @@
-import pytest
-from model_bakery import baker
-
 from pypro.modulos import facade
-from pypro.modulos.models import Modulo
-
-
-@pytest.fixture
-def modulos(db):
-    return baker.make(Modulo, 2)
 
 
 def test_listar_modulos_ordenados(modulos):
